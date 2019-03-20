d59785b3eda9d84e02809910315d05b5e70a8529
anderson.firmino <anderson.firmino@m4u.com.br>
2019-03-19 22:10:15 -0300

    Rename folder and added new scenarios.

--- exemplo_respec_tdd/lib/calculator.rb
+++
@@ -1,5 +0,0 @@
-class Calculator
-    def sum(num1, num2)
-        num1 + num2
-    end
-end
--- exemplo_respec_tdd/spec/calculator/calculator_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'calculator'
-
-describe Calculator do 
-    it 'use sum method for two numbers' do 
-        calc = Calculator.new
-        result = calc.sum(5,7)
-        expect(result).to eq(12) 
-    end
-end
