38240c9170583ecfb287cad462769cceb7411834
Anderson Firmino <anderson.firmino@m4u.com.br>
2019-04-16 14:54:13 -0300

    Added raise error handler

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,6 +1,25 @@
 require 'calculator'
 
 describe Calculator, 'Sobre a calculadora' do
+
+  context "#div" do
+    it 'raise_error with error class - divided by zero' do
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError)
+    end
+
+    it 'raise_error with error message - divided by zero' do
+      expect{subject.div(3,0)}.to raise_error("divided by 0")
+    end
+
+    it 'raise_error with error class and message - divided by zero' do
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError, "divided by 0")
+    end
+
+    it 'raise_error with regular expression - divided by zero' do
+      expect{subject.div(3,0)}.to raise_error(/divided/)
+    end
+  end
+
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5, 7)
