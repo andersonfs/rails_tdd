5e01fa9975018c510fcf29c3b2b43039f87d062b
Anderson Firmino <anderson.firmino@m4u.com.br>
2019-04-16 15:38:42 -0300

    Added on-liner syntax blocks.

--- exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
@@ -1,4 +1,8 @@
 describe (1..5), 'Ranges' do
+    it '#cover - with one element and On-liner syntax ' do 
+        is_expected.to cover(2)
+    end
+
     it '#cover - with one element' do 
         expect(subject).to  cover(2)
     end
@@ -10,4 +14,9 @@ describe (1..5), 'Ranges' do
     it '#cover | not to - with more than one element' do 
         expect(subject).not_to  cover(0,6)
     end
+
+    # On-liner Syntax blocks
+    it { is_expected.to cover(2) }
+    it { is_expected.to cover(2,5) }
+    it { is_expected.not_to cover(0,6) }
 end
